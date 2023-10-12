87421a999834e1961a3bb12668acd7aad02d5e97
Camilo Catasus <camilomcatasus@gmail.com>
2023-10-12 17:56:01 -0400

    Simulation Essentially works but decoder has broken

--- perfaware/sim86/sim86.cpp
+++ perfaware/sim86/sim86.cpp
@@ -21,13 +21,54 @@
 #include "sim86_text.cpp"
 #include "sim86_decode.cpp"
 
-void SimulateInstruction(sim_context SimContext, instruction Instruction) {
+u16 GetOperandValue(sim_context* SimContext, instruction_operand Operand) {
+    u16 return_val = 0x0;
+
+    switch (Operand.Type) {
+        case Operand_Register: {
+            u16 temp_val = SimContext->registers[Operand.Register.Index] >> (8 * Operand.Register.Offset);
+            if(Operand.Register.Count == 1)
+                temp_val = temp_val & 0b11111111;
+            return_val = temp_val;
+            
+        } break;
+        case Operand_Immediate: {
+            if (Operand.ImmediateS32 != NULL)
+                return_val = Operand.ImmediateS32;
+            else
+                return_val = Operand.ImmediateU32;
+            
+        } break;
+        default: break;
+    }
+
+    return return_val;
+}
+
+void SetRegisterValue(sim_context* SimContext, register_access RegisterAccess, u16 value) {
+    u16 temp_val = value;
+    u16 old_val = SimContext->registers[RegisterAccess.Index];
+    if(RegisterAccess.Count == 1)
+    {
+        SimContext->registers[RegisterAccess.Index] = SimContext->registers[RegisterAccess.Index] & (0b11111111 << (8 * (1 - RegisterAccess.Offset)));
+        SimContext->registers[RegisterAccess.Index] = SimContext->registers[RegisterAccess.Index] | (value << (8 * RegisterAccess.Offset));
+    }
+    else
+    {
+        SimContext->registers[RegisterAccess.Index] = value;
+    }
+    
+    printf(" ; 0x%x->0x%x", old_val, SimContext->registers[RegisterAccess.Index]);
+}
+
+void SimulateInstruction(sim_context* SimContext, instruction Instruction) {
     switch (Instruction.Op) {
         case Op_mov: {
             u8 base_register = 0;
+            u16 load_value = GetOperandValue(SimContext, Instruction.Operands[1]);
             switch (Instruction.Operands[0].Type) {
                 case Operand_Register: {
-
+                    SetRegisterValue(SimContext, Instruction.Operands[0].Register, load_value);
                 } break;
                 case Operand_Memory: {
 
@@ -42,7 +83,6 @@ void SimulateInstruction(sim_context SimContext, instruction Instruction) {
             break;
     }
 }
-
 static void DisAsm8086(memory *Memory, u32 DisAsmByteCount, segmented_access DisAsmStart)
 {
     segmented_access At = DisAsmStart;
@@ -50,6 +90,7 @@ static void DisAsm8086(memory *Memory, u32 DisAsmByteCount, segmented_access Dis
     disasm_context Context = DefaultDisAsmContext();
     sim_context SimContext = sim_context {}; 
     u32 Count = DisAsmByteCount;
+
     while(Count)
     {
         instruction Instruction = DecodeInstruction(&Context, Memory, &At);
@@ -67,13 +108,14 @@ static void DisAsm8086(memory *Memory, u32 DisAsmByteCount, segmented_access Dis
             }
             
             UpdateContext(&Context, Instruction);
-            SimulateInstruction(SimContext, Instruction);
 
             if(IsPrintable(Instruction))
             {
                 PrintInstruction(Instruction, stdout);
-                printf("\n");
             }
+            
+            SimulateInstruction(&SimContext, Instruction);
+            printf("\n");
         }
         else
         {
@@ -81,6 +123,8 @@ static void DisAsm8086(memory *Memory, u32 DisAsmByteCount, segmented_access Dis
             break;
         }
     }
+
+    PrintContext(SimContext);
 }
 
 
--- perfaware/sim86/sim86_simulate.h
+++ perfaware/sim86/sim86_simulate.h
@@ -3,23 +3,22 @@
 #include <stdio.h>
 
 struct sim_context {
-    u16 registers[11] = {0};
+    u16 registers[15] = {0};
 };
 
-static u16 registers[11] = {0};
 
 
 void PrintContext(sim_context simContext) {
-    printf("Registers");
-    printf("- ax: %u", simContext.registers[0]);
-    printf("- bx: %u", simContext.registers[1]);
-    printf("- cx: %u", simContext.registers[2]);
-    printf("- dx: %u", simContext.registers[3]);
-    printf("- sp: %u", simContext.registers[4]);
-    printf("- dp: %u", simContext.registers[5]);
-    printf("- si: %u", simContext.registers[6]);
-    printf("- di: %u", simContext.registers[7]);
-    printf("- es: %u", simContext.registers[8]);
-    printf("- ss: %u", simContext.registers[9]);
-    printf("- ds: %u", simContext.registers[10]);
+    printf("Registers\n");
+    printf("- ax: %#06x\n", simContext.registers[1]);
+    printf("- bx: %#06x\n", simContext.registers[2]);
+    printf("- cx: %#06x\n", simContext.registers[3]);
+    printf("- dx: %#06x\n", simContext.registers[4]);
+    printf("- sp: %#06x\n", simContext.registers[5]);
+    printf("- dp: %#06x\n", simContext.registers[6]);
+    printf("- si: %#06x\n", simContext.registers[7]);
+    printf("- di: %#06x\n", simContext.registers[8]);
+    printf("- es: %#06x\n", simContext.registers[9]);
+    printf("- ss: %#06x\n", simContext.registers[10]);
+    printf("- ds: %#06x\n", simContext.registers[11]);
 }
